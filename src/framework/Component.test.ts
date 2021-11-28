c360c1c7bec35578db358eceddbd2c9c262fe8f1
jungdujang <jungdujang@gmail.com>
2021-11-29 01:05:31 +0900

    Update: 컴포넌트의 state가 변경되었을 때 다시 렌더링 하는 기능 추가

--- src/framework/Component.test.ts
+++ src/framework/Component.test.ts
@@ -17,8 +17,43 @@ describe('Component', () => {
       }
     }
 
-    (new TestComponent()).render(rootDiv);
+    (new TestComponent(rootDiv)).render();
 
     expect(rootDiv.textContent).toBe("Hello World")
+  });
+
+  it('render component nested element', () => {
+    class TestComponent extends Component {
+      getView(){
+        return createVDom('ul', {}, [
+          createVDom('li', {}, "item1"),
+          createVDom('li', {}, "item2"),
+          createVDom('li', {}, "item3")
+        ])
+      }
+    }
+
+    (new TestComponent(rootDiv)).render();
+
+    expect(rootDiv.querySelectorAll('ul').length).toBe(1);
+    expect(rootDiv.querySelectorAll('li').length).toBe(3);
+  });
+
+  it('rerender component when state is changed', () => {
+    class TestComponent extends Component{
+      getView(){
+        const state = this.getState();
+        return createVDom('div', {}, `count: ${state.count || 0}`);
+      }
+    }
+    const testComponent = new TestComponent(rootDiv);
+    testComponent.render();
+    expect(rootDiv.querySelector('div')?.textContent).toBe("count: 0");
+
+    testComponent.setState({count: 100});
+    expect(rootDiv.querySelector('div')?.textContent).toBe("count: 100")
+
+    testComponent.setState({count: 1000});
+    expect(rootDiv.querySelector('div')?.textContent).toBe("count: 1000")
   })
 })
--- src/framework/Component.ts
+++ src/framework/Component.ts
@@ -1,12 +1,71 @@
-import { createHTMLElement, VDom } from "./vdom";
+import Eventing from "./Eventing";
+import { createHTMLElement, diff, dispatch, VDom } from "./vdom";
 
+type State = {
+  [key: string]: any
+}
+
+type SetStateCallback = (prevState: any) => any
+
+const STATE_CHANGE_EVENT_NAME = "STATE_CHANGE";
 export default abstract class Component {
-  abstract getView():VDom
+  protected state: any = {};
+  private eventing =  new Eventing();
+  private prevView: VDom | null = null;
 
-  render(parent: HTMLElement){
+  constructor(private parent: HTMLElement){
+    this.eventing.on(STATE_CHANGE_EVENT_NAME, () => {
+      this.rerender()
+    })
+  }
+
+  abstract getView():VDom;
+
+
+  //// State 관련 함수
+
+  setState(newState: State | SetStateCallback){
+    if(typeof newState === "function"){
+      this.state = newState(this.state);
+    }else{
+      this.state = newState;
+    }
+
+    this.eventing.trigger(STATE_CHANGE_EVENT_NAME)
+  }
+
+  getState(){
+    return this.state;
+  }
+
+
+  //// Render 관련 함수
+
+  private rerender(){
+    if(!this.prevView){
+      return new Error("Require prevView to rerender");
+    }
+
     const view = this.getView();
+    const diffAction = diff(view, this.prevView);
+    dispatch(this.parent, diffAction);
 
+    this.prevView = view;
+  }
+
+  private initRender(){
+    const view = this.getView();
     const elem = createHTMLElement(view);
-    parent.appendChild(elem);
+    this.parent.appendChild(elem);
+
+    this.prevView = view;
+  }
+
+  render(){
+    if(this.prevView){
+      this.rerender();
+    }else{
+      this.initRender()
+    }
   }
 }
